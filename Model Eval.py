0a73488d9a55f626d07693c97c474d6e30c8a6be
schimpfen <nathanschimpf@gmail.com>
2021-02-05 10:21:45 -0500

    Eval Draft

--- Model Eval.py
+++ Model Eval.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import torch, tqdm
 from model import CONV_LSTM, load_model
 from custom_dataset import CustomDataset, ToTensor
 # Load model and specified set of test flights
@@ -18,4 +19,33 @@ flight_data = CustomDataset(root_dir=root, transform=ToTensor(), device=dev)
 flight_data.validate_sets(underMin=100)
 
 # begin validation
-mdl.evaluate(flight_data, test_flights)
+flight_losses = torch.zeros(len(test_flights))
+for i in tqdm.trange(len(test_flights)):
+
+    fp, ft, wc = flight_data[test_flights[i]]
+    maxlen = min(len(fp), len(ft), len(wc))
+    fp, ft, wc = fp[:, 1:], ft[:, 1:], wc[:]
+
+    mdl.optimizer.zero_grad()
+    mdl.hidden_cell = (
+        torch.repeat_interleave(fp[0][0], mdl.lstm_hidden).view(1, 1, mdl.lstm_hidden),
+        torch.repeat_interleave(fp[0][1], mdl.lstm_hidden).view(1, 1, mdl.lstm_hidden))
+    y_pred = mdl(wc.reshape((-1, 1, 20, 20)), fp[:])
+    flight_losses[i] = mdl.loss_function(y_pred, ft[:].view(-1, 2))
+
+    if mdl.device.__contains__('cuda'):
+        y_pred, fp, ft = y_pred.cpu(), fp.cpu(), ft.cpu()
+    y_pred, fp, ft = y_pred.detach().numpy(), fp.detach().numpy(), ft.detach().numpy()
+
+    df_flight = pd.DataFrame(data={'flight plan LAT': fp[:,0], 'flight plan LON': fp[:,1],
+                                   'prediction LAT': y_pred[:,0], 'prediction LON': y_pred[:,1],
+                                   'actual LAT': ft[:,0], 'actual LON': ft[:,1]})
+    df_flight.to_csv('Output/eval {}'.format(flight_data.get_flightname(i)))
+
+if mdl.device.__contains__('cuda'):
+    flight_losses = flight_losses.cpu()
+flight_losses = flight_losses.detach().numpy()
+
+flight_names = [flight_data.get_flightname(x) for x in range(len(flight_losses))]
+df_losses = pd.DataFrame(data={'flight name': flight_names, 'loss (MSE)': flight_losses})
+df_losses.to_csv('Output/flight losses')
