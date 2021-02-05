25c3836c61dbe944f92474f55fd3649b2c2f7304
schimpfen <nathanschimpf@gmail.com>
2021-02-05 08:42:34 -0500

    Anticipated Training Methods

--- train.py
+++ train.py
@@ -9,6 +9,13 @@ from custom_dataset import ToTensor
 import torch.nn as nn
 from model import CONV_LSTM
 
+# TODO: BATCH FLIGHTS
+'''TRAINING CONFIGS
+3D EXPECTED: 1000 Epochs, Batch Size 64 -> MSE .001 Train/Test 90/10
+3D EXPECTED: 500 Epochs -> MSE .001 (no batching)   Train/Test 75/25
+4D EXPECTED: 500 Epochs -> MSE .03                  Train/Test 75/25
+
+'''
 
 dev='cuda:0'
 # dev = 'cpu'
@@ -19,12 +26,12 @@ flight_data = CustomDataset(root_dir, ToTensor(), dev)
 flight_data.validate_sets(underMin=100)
 
 total_flights = len(flight_data)
-train_flights = np.random.choice(total_flights, int(total_flights*.8), replace=False)
+train_flights = np.random.choice(total_flights, int(total_flights*.9), replace=False)
 test_flights =list(set(range(len(flight_data))) - set(train_flights))
 
 # train_model
 paradigms = {0: 'Regression', 1: 'Seq2Seq'}
-model = CONV_LSTM(paradigm=paradigms[1],device=dev) #customized Convolution and LSTM model
+model = CONV_LSTM(paradigm=paradigms[1],device=dev)
 # set training epochs and train
 epochs = 5
 sttime = datetime.now()
