0985cd4f1b6b894fa19b3386738ca1c026f05fc1
Nathan Schimpf <nathanschimpf@gmail.com>
2021-03-15 09:17:00 -0400

    OS-dependent features and Reporting error-check

--- Reporting.py
+++ Reporting.py
@@ -1,5 +1,8 @@
 import os
-os.environ['PROJ_LIB'] = "/home/lab/anaconda3/pkgs/proj-7.2.0-he47e99f_1/share/proj/"
+if os.name == 'posix':
+    os.environ['PROJ_LIB'] = "/home/lab/anaconda3/pkgs/proj-7.2.0-he47e99f_1/share/proj/"
+elif os.name == 'nt':
+    os.environ['PROJ_LIB'] = 'C:\\Users\\User\\anaconda3\\pkgs\\proj4-5.2.0-ha925a31_1\\Library\\share'
 import tqdm
 import numpy as np
 import pandas as pd
@@ -20,39 +23,41 @@ for mdl in mdls:
     for e in tqdm.trange(len(evals)):
         # query df for normalized loss
         mse_loss = df_flight_losses[df_flight_losses['flight name'] == evals[e][5:]]['loss (MSE)'].values
+        if len(mse_loss) > 0:
+            # calculated L2norm and % improvement for denormed flight
+            df_flight = pd.read_csv('Output/{}/Denormed/{}'.format(mdl,evals[e]))
+            nda_fp = df_flight[['flight plan LAT', 'flight plan LON']].values.T
+            nda_pred = df_flight[['predicted LAT', 'predicted LON']].values.T
+            nda_act =  df_flight[['actual LAT', 'actual LON']].values.T
+            l2_fps[e] = fn.L2Norm(nda_act, nda_fp)
+            l2_preds[e] = fn.L2Norm(nda_act, nda_pred)
+            reduction = fn.reduction(l2_fps[e], l2_preds[e])
 
-        # calculated L2norm and % improvement for denormed flight
-        df_flight = pd.read_csv('Output/{}/Denormed/{}'.format(mdl,evals[e]))
-        nda_fp = df_flight[['flight plan LAT', 'flight plan LON']].values.T
-        nda_pred = df_flight[['predicted LAT', 'predicted LON']].values.T
-        nda_act =  df_flight[['actual LAT', 'actual LON']].values.T
-        l2_fps[e] = fn.L2Norm(nda_act, nda_fp)
-        l2_preds[e] = fn.L2Norm(nda_act, nda_pred)
-        reduction = fn.reduction(l2_fps[e], l2_preds[e])
+            # plot trajectory v flight plan v actual, include mse, reduction in title and norms in legend
 
-        # plot trajectory v flight plan v actual, include mse, reduction in title and norms in legend
+            '''m = Basemap(width=12000000, height=9000000,
+                        area_thresh=10000, projection='lcc',
+                        lat_0=38., lon_0=-98., lat_1=25.)'''
+            m = Basemap(area_thresh=1000, projection='merc',
+                        llcrnrlon=-130, urcrnrlon=-60, llcrnrlat=20, urcrnrlat=45)
+            parallels = np.arange(0.,80.,10.)
+            meridians = np.arange(10.,351.,20.)
+            m.drawcoastlines()
+            m.drawparallels(parallels, labels=[False, True, True, False])
+            m.drawmeridians(meridians, labels=[True, False, False, True])
+            fig2 = plt.gca()
 
-        '''m = Basemap(width=12000000, height=9000000,
-                    area_thresh=10000, projection='lcc',
-                    lat_0=38., lon_0=-98., lat_1=25.)'''
-        m = Basemap(area_thresh=1000, projection='merc',
-                    llcrnrlon=-130, urcrnrlon=-60, llcrnrlat=20, urcrnrlat=45)
-        parallels = np.arange(0.,80.,10.)
-        meridians = np.arange(10.,351.,20.)
-        m.drawcoastlines()
-        m.drawparallels(parallels, labels=[False, True, True, False])
-        m.drawmeridians(meridians, labels=[True, False, False, True])
-        fig2 = plt.gca()
-
-        m.plot(nda_fp[1,:], nda_fp[0,:], latlon=True, color='blue',label='flight plan  L2-Norm:{:.3f}'.format(l2_fps[e]))
-        m.plot(nda_pred[1,:], nda_pred[0,:], latlon=True, color='yellow', label='prediction  L2-Norm:{:.3f}'.format(l2_preds[e]))
-        m.plot(nda_act[1,:], nda_act[0,:], latlon=True, color='red', label='actual')
-        plt.legend()
-        plt.title("{}\nMSE: {:.4f}   Reduction: {:.3f}".format(evals[e][5:-4], mse_loss[0], reduction * 100))
-        if not os.path.isdir('Output/{}/Figs'.format(mdl)):
-            os.mkdir('Output/{}/Figs'.format(mdl))
-        plt.savefig('Output/{}/Figs/{}.png'.format(mdl,evals[e]), dpi=300)
-        plt.close()
+            m.plot(nda_fp[1,:], nda_fp[0,:], latlon=True, color='blue',label='flight plan  L2-Norm:{:.3f}'.format(l2_fps[e]))
+            m.plot(nda_pred[1,:], nda_pred[0,:], latlon=True, color='yellow', label='prediction  L2-Norm:{:.3f}'.format(l2_preds[e]))
+            m.plot(nda_act[1,:], nda_act[0,:], latlon=True, color='red', label='actual')
+            plt.legend()
+            plt.title("{}\nMSE: {:.4f}   Reduction: {:.3f}".format(evals[e][5:-4], mse_loss[0], reduction * 100))
+            if not os.path.isdir('Output/{}/Figs'.format(mdl)):
+                os.mkdir('Output/{}/Figs'.format(mdl))
+            plt.savefig('Output/{}/Figs/{}.png'.format(mdl,evals[e]), dpi=300)
+            plt.close()
+        else:
+            print('MISSING {}'.format(evals[e]))
 
     # plot norm distributions (fp, pred) w/ % reduction in title
     reduction = fn.reduction(l2_fps, l2_preds)
